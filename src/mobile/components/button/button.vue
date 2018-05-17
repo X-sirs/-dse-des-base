f6d842815c913678ca5d6faae470bf7bc472e8df
nss <spallate@163.com>
2018-05-17 17:17:59 +0800

    优化button

--- src/mobile/components/button/button.vue
+++ src/mobile/components/button/button.vue
@@ -1,20 +1,48 @@
 <template>
-    <div @click="click" class="dse-button">
-        按钮
-    </div>
+    <button @click="click"  :style="styles" :disabled="disabled" :class="disabled?'dse-button disable':'dse-button enable'">
+        {{disabled?disablecontent:enablecontent}}
+    </button>
 </template>
 <script>
     export default {
-        name:'button',
-        methods:{
-            click(){
-                console.log("test package")
-            }
+        name:'DButton',
+        props:{
+            enablecontent:{
+                type:String,
+                default:"操作"
+            },
+            styles:{
+                type:String,
+                default:null
+            },
+            disablecontent:{
+                type:String,
+                default:"操作中..."
+            },
+            click:{
+                type:Function,
+                default:null
+            },
+            disabled:{
+                type:Boolean,
+                default:false
+            }  
         }
     }
 </script>
 <style scoped lang="less">
     .dse-button{
+        padding:2px 5px;
+        color: #fff;
+        text-align: center;
+        border-radius: 3px;
+        border: none;
+        outline: none;
+    }
+    .enable{
         background-color: #0085ff;
+    }
+    .disable{
+        background-color: #ddd;
     }
 </style>
